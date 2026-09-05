611efb705c8059877bb328232f3c535c1132e0f6
sdelrio <sdelrio@users.noreply.github.com>
2026-09-05 12:29:51 +0200

    feat(theme-1-core): set light mode as default theme (#386)
    Closes #367

--- docs-site/astro.config.mjs
+++ docs-site/astro.config.mjs
@@ -21,6 +21,9 @@ export default defineConfig({
       customCss: [
         './src/styles/custom.css',
       ],
+      components: {
+        ThemeProvider: './src/components/ThemeProvider.astro',
+      },
       plugins: [
         starlightLinksValidator(),
       ],
